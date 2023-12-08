3cae954ba3b56586bcf3fd53fba139cbb22dd1f1
cloud9cloud9 <nalezhitiyvlad@gmail.com>
2023-12-08 23:11:49 +0200

    add hw about using reflection

--- reflection/src/main/java/com/alevel/StudentTest.java
+++ reflection/src/main/java/com/alevel/StudentTest.java
@@ -1,7 +1,25 @@
 package com.alevel;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class StudentTest {
     public static void main(String[] args) {
-        // виконання дз
+        Student student = new Student("Vlad", "Luthor", 21, 95);
+        StudentInfoPrinter studentInfoPrinter = new StudentInfoPrinter();
+        studentInfoPrinter.printStudentInfo(student);
+
+        try {
+            Method method = student.getClass().getDeclaredMethod("passExamSuccessfully");
+            method.setAccessible(true);
+            method.invoke(student);
+            Field field = student.getClass().getDeclaredField("examScore");
+            field.setAccessible(true);
+            Object o = field.get(student);
+            System.out.println("new exam Score of that students : " + o);
+        } catch (InvocationTargetException | IllegalAccessException | NoSuchMethodException | NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
