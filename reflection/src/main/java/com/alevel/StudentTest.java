9803a0363f35ed84acb44a7ce3284bfc4573d857
cloud9cloud9 <nalezhitiyvlad@gmail.com>
2023-12-29 21:02:13 +0200

    add correction in hw about using jdbc

--- reflection/src/main/java/com/alevel/StudentTest.java
+++ reflection/src/main/java/com/alevel/StudentTest.java
@@ -6,20 +6,20 @@ import java.lang.reflect.Method;
 
 public class StudentTest {
     public static void main(String[] args) {
-        Student student = new Student("Vlad", "Luthor", 21, 95);
-        StudentInfoPrinter studentInfoPrinter = new StudentInfoPrinter();
-        studentInfoPrinter.printStudentInfo(student);
-
-        try {
-            Method method = student.getClass().getDeclaredMethod("passExamSuccessfully");
-            method.setAccessible(true);
-            method.invoke(student);
-            Field field = student.getClass().getDeclaredField("examScore");
-            field.setAccessible(true);
-            Object o = field.get(student);
-            System.out.println("new exam Score of that students : " + o);
-        } catch (InvocationTargetException | IllegalAccessException | NoSuchMethodException | NoSuchFieldException e) {
-            throw new RuntimeException(e);
-        }
+//        Student student = new Student("Vlad", "Luthor", 21, 95);
+//        StudentInfoPrinter studentInfoPrinter = new StudentInfoPrinter();
+//        studentInfoPrinter.printStudentInfo(student);
+//
+//        try {
+//            Method method = student.getClass().getDeclaredMethod("passExamSuccessfully");
+//            method.setAccessible(true);
+//            method.invoke(student);
+//            Field field = student.getClass().getDeclaredField("examScore");
+//            field.setAccessible(true);
+//            Object o = field.get(student);
+//            System.out.println("new exam Score of that students : " + o);
+//        } catch (InvocationTargetException | IllegalAccessException | NoSuchMethodException | NoSuchFieldException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 }
